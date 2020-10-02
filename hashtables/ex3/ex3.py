417d76e90796825daf1aa41a862d657425483ab9
numbers0580 <koreankid18@yahoo.com>
2020-10-02 10:40:41 -0600

    Completed ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,7 +2,31 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # [
+    #     [1, 2, 3, 4, 5],
+    #     [12, 7, 2, 9, 1],
+    #     [99, 2, 7, 1,]
+    # ]
+    # returned output: [1, 2]
+    # The above nested list is from the README
+
+    cache = {}
+    result = []
+
+    for nestedlist in arrays:
+        # first iteration focuses on [1, 2, 3, 4, 5]
+        for item in nestedlist:
+            # first item is 1
+            if item in cache:
+                # Increment the existing count
+                cache[item] += 1
+            else:
+                # Start the count for each value in the entire arrays
+                cache[item] = 1
+
+            # Now I want to see which items have a count total matching the number of nested lists (in above case: 3)
+            if cache[item] == len(arrays):
+                result.append(item)
 
     return result
 
