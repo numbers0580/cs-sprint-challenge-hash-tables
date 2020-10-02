fa220fa6b848042b0b3fc57c431085684ad06cb0
numbers0580 <koreankid18@yahoo.com>
2020-10-02 10:28:25 -0600

    Completed ex1 & ex2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,26 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # Comments will follow the weights = [4, 6, 10, 15, 16], length = 5, limit = 21 example
+
+    cache = {}
+
+    #length = 5
+    for i in range(length):
+        #limit = 21. target should be 17, 15, 11, 6, 5 when i = 0, 1, 2, 3, 4
+        target = limit - weights[i]
+        if target in cache:
+            # first target this should find is 6. See comments in else section
+            x = cache[target] # x = 1
+            pairing = [i, x] # pairing = [3, 1]
+            # due to subtraction, there's no way to know which if i had been the larger or smaller index, so sort largest to smallest
+            pairing.sort(reverse = True)
+
+            return pairing
+        else:
+            # targets of 17, 15, 11 would not be found in cache
+            # since the output needs the position of the values, store the position (i) to the cache
+            # when target is 15, i = 1, and weights[i] = 6, so cache[6] = 1
+            cache[weights[i]] = i
 
     return None
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,25 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # tickets = [
+    #     Ticket{ source: "III", destination: "OOO" },
+    #     Ticket{ source: "AAA", destination: "NONE" },
+    #     Ticket{ source: "NONE", destination: "III" },
+    #     Ticket{ source: "OOO", destination: "AAA" }
+    # ]
+    # the above array is for visualization. Goal is to return ["III", "OOO", "AAA"]. Notice the "NONE"s are removed
+    # Just double-checked the tests. The README removed both "NONE"s, but the tests expects the destination "NONE" to remain
+
+    cache = {}
+    route = []
+
+    for i in tickets:
+        cache[i.source] = i.destination
+        # We should have cache["NONE"] = "III", cache["III"] = "OOO", cache["OOO"] = "AAA", cache["AAA"] = "NONE"
+
+    airport = cache["NONE"] # Initialization for the for-loop below
+    for j in range(len(tickets)):
+        route.append(airport)
+        airport = cache[airport]
 
     return route
